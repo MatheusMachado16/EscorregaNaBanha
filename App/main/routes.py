eae2df7965b470b89cdf5de6749dca7d748260d2
Matheus Yan Machado Santos <202514320002@ad.ifpb.edu.br>
2025-08-08 20:22:31 -0300

    Blueprint

---
+++ App/main/routes.py
@@ -0,0 +1,32 @@
+from flask import render_template, current_app, Flask, request, session
+from . import main
+
+# @main.route('/')
+# def index():
+#     posts = current_app.posts
+#     return render_template('index.html', posts=posts)
+
+@main.route("/")
+def index():
+    posts = current_app.posts
+    return render_template("index.html", posts=posts)
+
+
+@main.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@main.route("/avaliacoes")
+def avaliacao():
+    return render_template("avaliacao.html")
+
+@main.route("/cardapio")
+def cardapio():
+    return render_template("cardapio.html")
+
+@main.route("/base")
+def base():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app.py
+++ app.py
@@ -1,30 +1,6 @@
-from flask import Flask, render_template, request, session
+from App import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/contato")
-def contato():
-    return render_template("contato.html")
-
-@app.route("/sobre")
-def sobre():
-    return render_template("sobre.html")
-
-@app.route("/avaliacoes")
-def avaliacao():
-    return render_template("avaliacao.html")
-
-@app.route("/cardapio")
-def cardapio():
-    return render_template("cardapio.html")
-
-@app.route("/base")
-def base():
-    return render_template("base.html")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
