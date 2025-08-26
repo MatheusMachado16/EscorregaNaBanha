c244a7ce4d66506abbb28f56022e3ed34a05bd31
MatheusMachado16 <yanm66733@gmail.com>
2025-08-26 20:18:08 -0300

    Deploy config

--- App/__init__.py
+++ App/__init__.py
@@ -1,9 +1,21 @@
+# from flask import Flask
+# from .main import main as main_bp
+
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.register_blueprint(main_bp)
+
+#     return app
+
 from flask import Flask
 from .main import main as main_bp
 
-
 def create_app():
-    app = Flask(__name__)
+    # Define onde estão os templates e arquivos estáticos
+    app = Flask(__name__, template_folder="App/templates", static_folder="App/static")
+    
+    # Registra o blueprint
     app.register_blueprint(main_bp)
 
     return app
