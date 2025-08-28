8baef8188d1e3acbfafb30b9ffe7109ae687a443
Odilon <odilonpontes7@gmail.com>
2025-08-28 12:13:48 -0300

    ajeitando avaliação

--- App/__init__.py
+++ App/__init__.py
@@ -4,7 +4,7 @@ from .main import main as main_bp
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "minha_chave_secreta_super_segura_123"
+    app.secret_key = 'sua_chave_secreta_aqui'
     app.register_blueprint(main_bp)
 
     return app
--- App/main/forms.py
+++ App/main/forms.py
@@ -10,13 +10,13 @@ CSV_PATH = os.path.join(os.path.dirname(__file__), "..",
 CSV_PATH = os.path.abspath(CSV_PATH)
 
 
-def salvar_csv(title, email, content, data_envio):
+def salvar_csv(title, email, content, nota, data_envio):
     file_exists = os.path.isfile(CSV_PATH)
     with open(CSV_PATH, mode='a', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         if not file_exists:
-            writer.writerow(['Title', 'Email', 'Content'])
-        writer.writerow([title, email, content, data_envio])
+            writer.writerow(['Title', 'Email', 'Content', 'nota'])
+        writer.writerow([title, email, content, data_envio, nota])
 
 
 def ler_csv():
@@ -28,15 +28,15 @@ def ler_csv():
                 posts.append(row)
     return posts
 
-
 @main.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         title = request.form['title']
         email = request.form['email']
         content = request.form['comentario']
+        nota = int(request.form['nota'])
         data_envio = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        salvar_csv(title, email, content, data_envio)
+        salvar_csv(title, email, content, nota, data_envio)
         flash("Avaliação enviada!", "success")
         return redirect(url_for('main.index'))
     return render_template("index.html")
@@ -46,4 +46,14 @@ def index():
 def avaliacao():
     posts = ler_csv()
     posts.reverse()
-    return render_template('avaliacao.html', posts=posts)
+    media = 0
+    notas = []
+    for post in posts: 
+        for chave in post:
+            if chave == 'nota':
+                x = int(post[chave])
+                notas.append(x)
+    qtd = len(notas)
+    if notas:
+        media = round(sum(notas)/qtd, 1)
+    return render_template('avaliacao.html', posts=posts, media=media, qtd=qtd)
